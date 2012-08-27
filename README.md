a4329d703b09e0558d0d41c61f038364512e7ed2
Deepak Kannan <kannan.deepak@gmail.com>
2012-08-27 16:48:12 +0530

    add rails_ui.rb application template

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ bootstrap
 =========
 
 starter scripts to bootstrap a project
+
+== script
+
+- rails_ui.rb
+  rails application template to create a rails application for UI
+  http://guides.rubyonrails.org/generators.html#customizing-your-workflow-by-changing-generators-templates
+
+  check out Dr. Nic's http://appscrolls.org, 
+  https://github.com/drnic/appscrolls
+  appscrolls is ambitious while these scripts are dumb
+
+
+NOTE:
+am trying to optimize my workflow and these scripts are quite fresh. 
+patches welcome
+
---
+++ rails_ui.rb
@@ -0,0 +1,103 @@
+# USAGE:
+# rails_new some_app_name ~/.bootstrap/rails_ui.rb
+# put the contents of this folder under ~/.bootstrap/
+
+# TODO: am not able to change the rails CLI options
+# encoding rails new <app_name> --skip-bundle -d postgresql -T"
+# @options = @options.dup.merge("skip_bundle" => true,
+#                               "database" => "postgresql",
+#                               "skip_test_unit" => true,
+#                               "skip_gemfile" => true)
+
+# so have a bash wrapper function
+# function rails_new()
+# {
+#   if [ -z "$1" ] #zero length?
+#   then
+#     echo "[ERROR] please provide app name. usage: rails_new <app-name>"
+#     return 1;
+#   else
+#     app_name=$1;
+#     if [ -z "$2" ] #zero length?
+#     then
+#         # --skip-bundle as we will do that after creating a .rvmrc
+#         # -d postgresql, use postgres as sqlite/mysql is the default
+#         # -T skip Test::Unit as will use rspec
+#         cmd="rails new ${app_name} --skip-bundle --skip-gemfile -d postgresql -T"
+#     else
+#         template_path=$2;
+#         # --skip-bundle as we will do that after creating a .rvmrc
+#         # -d postgresql, use postgres as sqlite/mysql is the default
+#         # -T skip Test::Unit as will use rspec
+#         cmd="rails new ${app_name} --skip-bundle --skip-gemfile -d postgresql -T -m ${template_path}"
+#     fi
+#     echo $cmd
+#     $cmd
+#     wait
+#   fi
+# }
+
+def silence_stream(out=STDOUT)
+  f = File::open('/dev/null', 'w')
+  orig = out.clone
+  out.reopen(f)
+ensure
+  out.reopen(orig)
+end
+
+def git_commit msg
+  silence_stream do
+    git :commit => %Q{ -m #{msg} }
+  end
+  log :run, msg
+end
+
+say ">--------------------------------[ INFO ]---------------------------------<"
+say "rails cmd line options"
+say @options.inspect
+say "app_name: #{app_name.inspect}"
+say "pwd: #{Dir.pwd}"
+say ">-------------------------------------------------------------------------<"
+
+copy_file "~/.bootstrap/rails/base_ui/Gemfile", "Gemfile"
+copy_file "~/.bootstrap/rails/base_ui/Gemfile.lock", "Gemfile.lock"
+run "cp -f ~/.bootstrap/rails/base_ui/dot_gitignore .gitignore"
+
+# `rvm --rvmrc --create ruby-1.9.3-p194@#{app_name}`
+copy_file "~/.bootstrap/rails/base_ui/dot_rvmrc", ".rvmrc"
+gsub_file '.rvmrc', /GIVE_A_GEMSET/, app_name
+
+run "mv README.rdoc doc/RAILS_README.rdoc"
+create_file "README.md", <<-README
+the .rvmrc has been changed (per project)
+
+TODO:
+1. check that rvm gemset should not exist or be empty
+2. change ~/.gemrc to
+   install: --no-rdoc --no-ri 
+   update:  --no-rdoc --no-ri
+3. gem install bundler
+4. bundle install
+5. git commit
+5. rake db:migrate
+6. git commit
+7. add project specific README
+README
+
+git :init
+git :add => "."
+git_commit 'First commit'
+# git :add => "onefile.rb", :rm => "badfile.cxx"
+# # remove_file "public/index.html"
+
+# TODO: cannot seem to reload rvmrc inside this script
+
+# say "Running Bundler install"
+# run 'bundle install'
+# git :add => "."
+# git_commit 'bundle install'
+
+# say "Running db:migrate"
+# rake("db:migrate")
+# git :add => "."
+# git_commit 'db migrate'
